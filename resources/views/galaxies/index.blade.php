8bc2e18faeaa5eebd6fc5e275fd417a846337787
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-05-03 15:59:47 +0200

    backoffice

--- resources/views/galaxies/index.blade.php
+++ resources/views/galaxies/index.blade.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>galaxies index</h1>
-    @dd($galaxies)
-</body>
-</html>
+@extends("layouts.galaxies")
+
+@section("title","The Cosmic Archive")
+
+
+@section('content')
+    
+   @foreach($galaxies as $galaxy)
+   <div class="card" style="width: 18rem;">
+
+  <img src="..." class="card-img-top" alt="...">
+
+  <div class="card-body">
+
+    <h5 class="card-title">Card title</h5>
+
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    
+  </div>
+</div>
+   @endforeach
+@endsection
